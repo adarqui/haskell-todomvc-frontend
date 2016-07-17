1504c4a347ee8cda6c4fea84db5e6563b80af00f
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-16 20:41:40 -0400

    main

---
+++ src/Web/TodoMVC/Frontend/ReactFlux/Main.hs
@@ -0,0 +1,22 @@
+module Web.TodoMVC.Frontend.ReactFlux.Main (
+  reactFluxMain
+) where
+
+
+
+import           React.Flux
+import           Web.TodoMVC.Frontend.ReactFlux.TodoView
+
+
+
+-- * reactRender :: Typeable props => String -> ReactView props -> props -> IO ()
+-- * todoApp :: ReactView ()
+--
+reactFluxMain :: IO ()
+reactFluxMain = do
+  reactRender
+    "todoapp" -- * String
+    todoApp   -- * ReactView props
+              -- * ReactView ()
+    ()        -- * props
+              -- * ()
